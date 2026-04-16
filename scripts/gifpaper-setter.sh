3b81245b24367b173618299fc1b72246855b0f0d
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-04-16 05:35:44 -0500

    Refactor gifpaper setter script
    Refactor gifpaper setter script to improve error handling and local variable usage.

--- scripts/gifpaper-setter.sh
+++ scripts/gifpaper-setter.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
+set -euo pipefail
 # gifpaper setter
 # simple utility for using 'mpvpaper' to spawn one of our lain animated wallpapers in navi
-t="gifpaper setter"; ver="0.50"
-gifpaper="$HOME/wiredWM/wp/gifpaperslain/lain4.gif"
-# funcs
 err_handler () {
   # simple error handler
   local err1="Oops! Something went wrong."
@@ -13,10 +11,13 @@ err_handler () {
 }
 run_gifpaper () {
   # selects the gifpaper specified above and runs it as animated wallpaper
+  # sends it to all detected displays for consistency
+  local gifpaper="$HOME/wiredWM/wp/gifpaperslain/lain4.gif"
   mpvpaper ALL -o "loop panscan=1" "$gifpaper"
 }
 main () {
   # main func
+  local t="gifpaper setter"; local ver="0.50"
   echo "$t - version $ver"; sleep 1
   echo "Setting animated wallpaper..."; sleep 1
   run_gifpaper
@@ -25,3 +26,5 @@ main () {
 }
 # - - - entry
 main || err_handler
+# TODO add checks to make sure mpvpaper is actually built and installed
+# run an installer and build script for it if not  
