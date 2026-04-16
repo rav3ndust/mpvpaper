7e8e5ec1d2cf3cee85465e56618af9730c13d82c
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-04-16 06:18:55 -0500

    Update build_install.sh

--- scripts/build_install.sh
+++ scripts/build_install.sh
@@ -13,9 +13,9 @@ clone_build_mpvpaper () {
 	 # clones and builds our mpvpaper fork repo, then installs it
 	 local repo_link="https://github.com/rav3ndust/mpvpaper"
 	 local pkgs="meson libmpv-dev"
- 	 doas apt update && doas apt install $pkgs -y
+ 	 doas apt update && doas apt install "$pkgs" -y
 	 # clone step
-  	 git clone $repo_link
+  	 git clone --single-branch "$repo_link"
   	 cd mpvpaper
 	 # build step
 	 local pre="/usr/local"; meson setup build --prefix="$pre"
@@ -27,17 +27,15 @@ run_gifpaper_setter () {
 	# runs the gifpaper_setter script we wrote for setting an animated wallpaper
 	local script="gifpaper_setter.sh"
 	echo "Setting animated wallpaper..."; sleep 1
-	bash $script
+	bash "$script"
 }
 main () {
 	local t="mpvpaper build and install script"
   	local ver="0.10"
  	echo "$t - version $ver"; sleep 1
-  	echo "Ensuring all needed packages are installed..."; sleep 1
-  	install_needed_pkgs
   	echo "Cloning, building, and installing mpvpaper..."; sleep 1
-  	clone_build_mpvpaper	# build and install
-	run_gifpaper_setter	    # set ani wp
+  	clone_build_mpvpaper		# build and install
+	run_gifpaper_setter		# set ani wp
 	echo "Done! You should now see your animated wallpaper."; sleep 1
 	echo "Exiting the script..."; sleep 3 && exit
 }
